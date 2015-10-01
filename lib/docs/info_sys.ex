2cdbc663f4688350d229144e640ff9129a632589
Brandon Richey <brichey@teladoc.com>
2015-10-01 15:10:55 -0500

    No idea where I deviated

---
+++ lib/docs/info_sys.ex
@@ -0,0 +1,36 @@
+defmodule Docs.InfoSys do
+  @backends [Doc.InfoSys.Wolfram]
+
+  def start_link(backend, opts) do
+    backend.start_link(opts)
+  end
+
+  def compute_img(expr) do
+    @backends
+    |> Enum.map(fn backend ->
+      Supervisor.start_child(Docs.InfoSys.Supervisor, [backend, [
+        client_pid: self,
+        expr: expr
+      ]])
+    end)
+    |> IO.inspect
+    |> Enum.map(&receive_results(&1))
+    |> Enum.filter(&(&1))
+    |> Enum.sort_by(fn result -> result.score end, &>/2)
+  end
+
+  defp receive_results({ref, pid}) do
+    result = receive do
+      {:result, ^pid, result} -> result
+      {:noresult, ^pid, _result} -> nil
+      {:DOWN, _ref, :process, ^pid, _} -> nil
+    after 5000 -> nil
+    end
+    Process.demonitor(ref)
+    receive do
+      {:DOWN, _ref, :process, ^pid, _} -> nil
+    after 0 -> nil
+    end
+    result
+  end
+end
